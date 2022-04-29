a70b6c37f10d9fcbf85a71c6e96f0ecba8215f1e
Ariel Davis <ariel.z.davis@icloud.com>
2022-04-28 23:18:20 -0700

    Start

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a href="https://reactjs.org">Learn React</a>
+    </div>
+  );
+}
